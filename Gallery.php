74219fad012b75b8794a2168098b23ddac2283a1
JimmyWhite86 <andrea.bianchi086@gmail.com>
2024-11-20 14:19:40 +0100

    Gallery con caroselll
    non funziona ancora come vorrei

--- Gallery.php
+++ Gallery.php
@@ -16,14 +16,7 @@
   <meta name="description" content="Associazione Culturale Stranamore">
   <meta name="author" content="Bianchi Andrea">
 
-  <!-- CDN CSS BOOTSTRAP -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <!-- JavaScript di Bootstrap -->
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+   
 
   <!-- Collegamento al mio file CSS -->
   <link href="base_css.css" rel="stylesheet" type="text/css">
@@ -55,7 +48,7 @@
 <!-- "Titolo" della pagina -->
 <div class="my-5 row justify-content-center">
   <div class="text-center">
-    <h1 class="titoloPagina">gallery fotografica</h1>
+    <h1 class="titoloPagina" id="titoloPaginaGallery">gallery fotografica</h1>
   </div>
 </div>
 
@@ -135,7 +128,7 @@
 
   <!-- Modale con carosello -->
   <div class="modal fade" id="carouselModal" tabindex="-1" aria-labelledby="carouselModalLabel" aria-hidden="true">   <!-- Nota 00 -->
-    <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
       <div class="modal-content">
         <div class="modal-header bg-giallo text-center justify-content-center align-item-center">
           <h5 class="modal-title fontTitoloCarosello" id="carouselModalLabel">le foto di strana</h5>
@@ -146,22 +139,22 @@
           <div id="galleryCarousel" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <img src="Immagini/ApeGiardino01.JPG" class="d-block w-100" alt="Persone fanno aperitivo nel giardino del circolo Stranamore">
+                <img src="Immagini/ApeGiardino01.JPG" class="d-block img-fluid" alt="Persone fanno aperitivo nel giardino del circolo Stranamore">
               </div>
               <div class="carousel-item">
-                <img src="Immagini/Donca_01.jpg" class="d-block w-100" alt="Concerto dei Doncaetano nella cantina del circolo Stranamore">
+                <img src="Immagini/Donca_01.jpg" class="d-block img-fluid" alt="Concerto dei Doncaetano nella cantina del circolo Stranamore">
               </div>
               <div class="carousel-item">
-                <img src="Immagini/ConcertoGiardino02.jpg" class="d-block w-100" alt="Concerto nel giardino del circolo Stranamore">
+                <img src="Immagini/ConcertoGiardino02.jpg" class="d-block img-fluid" alt="Concerto nel giardino del circolo Stranamore">
               </div>
               <div class="carousel-item">
-                <img src="Immagini/Dibattito01.jpg" class="d-block w-100" alt="Dibattito al circolo Stranamore">
+                <img src="Immagini/Dibattito01.jpg" class="d-block img-fluid" alt="Dibattito al circolo Stranamore">
               </div>
               <div class="carousel-item">
-                <img src="Immagini/Boiler01.jpg" class="d-block w-100" alt="Aperitivo con DJ Set nel giardino del circolo Stranamore">
+                <img src="Immagini/Boiler01.jpg" class="d-block img-fluid" alt="Aperitivo con DJ Set nel giardino del circolo Stranamore">
               </div>
               <div class="carousel-item">
-                <img src="Immagini/CenaGiardino01.jpg" class="d-block w-100" alt="Cena sociale nel giardino del circolo Stranamore">
+                <img src="Immagini/CenaGiardino01.jpg" class="d-block img-fluid" alt="Cena sociale nel giardino del circolo Stranamore">
               </div>
             </div>
           </div>
