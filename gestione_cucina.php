e6e3b7d6dea32ed22e7ee5ab65c1a8ac942e64b8
JimmyWhite86 <andrea.bianchi086@gmail.com>
2025-02-01 12:16:02 +0100

    Lavoro su accessibilità _ 05
    Ho iniziato a lavorare sull'accessibilità delle pagine da amministratore

--- gestione_cucina.php
+++ gestione_cucina.php
@@ -14,34 +14,50 @@
 
 <body>
 
-<?php $userName = $_SESSION['username']; ?>
-
 <!-- Funzione per creare dinamicamente la NavBar -->
 <?php richiamaNavBar($nomePagina) ?>
 
+<main id="mioMain">
 
-<!-- "Titolo" della pagina -->
-<div class="my-5 row justify-content-center">
-  <div class="text-center">
-    <h1 class="titoloPagina">gestione cucina</h1>
-  </div>
-</div>
-
-<div class="container-fluid bg-rosso pb-4 pt-4 mt-4 mb-4">
-  <div class="container-fluid col-md-8 bg-bianco pb-4 mb-4 pt-4 mt-4">
-    <div class="row justify-content-center">
-      <h2 class="text-center">Ciao <?= $userName ?>, scegli un azione:</h2>
-      <ul class="list-unstyled ml-5 pl-5">
-        <li><a href="<?= BASE_URL ?>/gestione_cucina/nuovo_menu_00.php">Crea un nuovo menù</a></li>
-        <li><a href="<?= BASE_URL ?>/gestione_cucina/elimina_menu.php">Elimina il menu presente</a></li>
-        <li><a href="<?= BASE_URL ?>/gestione_cucina/crea_piatto.php">Aggiungi un singolo piatto al menu</a></li>
-        <li><a href="<?= BASE_URL ?>/gestione_cucina/elimina_piatto.php">Elimina un singolo piatto dal menu</a></li>
-        <li><a href="<?= BASE_URL ?>/gestione_cucina/modifica_piatto_00.php">Modifica un singolo piatto del menu</a></li>
-      </ul>
+  <!-- "Titolo" della pagina -->
+  <div class="my-5 row justify-content-center">
+    <div class="text-center">
+      <h1 class="titoloPagina">gestione cucina</h1>
     </div>
   </div>
-</div>
+  
+  <?php
+    if (!isset($_SESSION["username"])) {
+      deviLoggarti();
+    } else {
+        $amministratore = $_SESSION["admin"];
+        $userName = $_SESSION["username"];
+    
+        if ($amministratore == 0) {
+        deviEssereAdmin($userName);
+        } else { ?>
 
+            <div class="container-fluid bg-rosso pb-4 pt-4 mt-4 mb-4">
+              <div class="container-fluid col-md-8 bg-bianco pb-4 mb-4 pt-4 mt-4">
+                <div class="row justify-content-center">
+                  <h2 class="text-center">Ciao <?php /*= $userName */?>, scegli un azione:</h2>
+                  <ul class="list-unstyled ml-5 pl-5">
+                    <li><a href="<?= BASE_URL ?>/gestione_cucina/nuovo_menu_00.php">Crea un nuovo menù</a></li>
+                    <li><a href="<?= BASE_URL ?>/gestione_cucina/elimina_menu.php">Elimina il menu presente</a></li>
+                    <li><a href="<?= BASE_URL ?>/gestione_cucina/crea_piatto.php">Aggiungi un singolo piatto al menu</a></li>
+                    <li><a href="<?= BASE_URL ?>/gestione_cucina/elimina_piatto.php">Elimina un singolo piatto dal menu</a></li>
+                    <li><a href="<?= BASE_URL ?>/gestione_cucina/modifica_piatto_00.php">Modifica un singolo piatto del menu</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+</main>
+
+      <?php
+        }
+     }
+      ?>
 
 <!-- Funzione per creare dinamicamente il footer -->
 <?php HTMLfooter($nomePagina) ?>
--- home_admin.php
+++ home_admin.php
@@ -14,56 +14,78 @@
 
 <body>
 
-<?php $userName = $_SESSION['username'] ?>
+<?php /*$userName = $_SESSION['username'] */?>
 
 <!-- Funzione per creare dinamicamente la NavBar -->
 <?php richiamaNavBar($nomePagina) ?>
 
-<!-- TODO: Manca controllo utente come admin -->
+<main id="mioMain">
 
-<!-- "Titolo" della pagina -->
-<div class="my-5 row justify-content-center">
-  <div class="text-center">
-    <h1 class="titoloPagina">home page admin</h1>
-  </div>
-</div>
+  <!-- TODO: Manca controllo utente come admin -->
 
-<section>
-  <div class="container-fluid bg-rosso">
-    <div class="row justify-content-center">
-      <!--      <h2 class="text-center m-3">, scegli un'azione </h2>-->
-
-      <div class="bg-bianco m-3 p-3 col-md-4 col-lg-3 col-11">
-        <h2 class="text-center">Gestione Cucina</h2>
-        <ul class="list-unstyled">
-          <li><a href="<?= BASE_URL ?>/gestione_cucina/nuovo_menu_00.php">Crea un nuovo menù</a></li>
-          <li><a href="<?= BASE_URL ?>/gestione_cucina/elimina_menu.php">Elimina il menu presente</a></li>
-          <li><a href="<?= BASE_URL ?>/gestione_cucina/crea_piatto.php">Aggiungi un singolo piatto al menu</a></li>
-          <li><a href="<?= BASE_URL ?>/gestione_cucina/elimina_piatto.php">Elimina un singolo piatto dal menu</a></li>
-          <li><a href="<?= BASE_URL ?>/gestione_cucina/modifica_piatto_00.php">Modifica un singolo piatto del menu</a></li>
-        </ul>
-      </div>
-
-      <div class="bg-bianco m-3 p-3 col-md-4 col-lg-3 col-11">
-        <h2 class="text-center">Gestione Eventi</h2>
-        <ul class="list-unstyled">
-          <li><a href="<?= BASE_URL ?>/gestione_eventi/crea_evento.php">Crea un nuovo evento</a></li>
-          <li><a href="<?= BASE_URL ?>/gestione_eventi/elimina_evento.php">Elimina un evento</a></li>
-          <li><a href="<?= BASE_URL ?>/gestione_eventi/modifica_evento_00.php">Modifica un evento</a></li>
-        </ul>
-      </div>
-
-      <div class="bg-bianco m-3 p-3 col-md-4 col-lg-3 col-11">
-        <h2 class="text-center">Gestione Utenti</h2>
-        <ul class="list-unstyled">
-          <li><a href="<?= BASE_URL ?>/gestione_utenti/crea_utente.php">Crea un nuovo utente con privilegi da Admin</a></li>
-          <li><a href="<?= BASE_URL ?>/gestione_utenti/elimina_utente.php">Elimina un utente</a></li>
-        </ul>
-      </div>
-
+  <!-- "Titolo" della pagina -->
+  <div class="my-5 row justify-content-center">
+    <div class="text-center">
+      <h1 class="titoloPagina">home page admin</h1>
     </div>
   </div>
-</section>
+  
+  <?php
+    if (!isset($_SESSION["username"])) {
+      deviLoggarti();
+    } else {
+        $amministratore = $_SESSION["admin"];
+        $username = $_SESSION["username"];
+    
+        if ($amministratore == 0) {
+        deviEssereAdmin($username);
+        } else { ?>
+
+  <section>
+    <div class="container-fluid bg-rosso">
+      <div class="row justify-content-center">
+
+        <div class="bg-bianco m-3 p-3 col-md-4 col-lg-3 col-11">
+          <h2 class="text-center">Gestione Cucina</h2>
+          <ul class="list-unstyled">
+            <li><a href="<?= BASE_URL ?>/gestione_cucina/nuovo_menu_00.php">Crea un nuovo menù</a></li>
+            <li><a href="<?= BASE_URL ?>/gestione_cucina/elimina_menu.php">Elimina il menu presente</a></li>
+            <li><a href="<?= BASE_URL ?>/gestione_cucina/crea_piatto.php">Aggiungi un singolo piatto al menu</a></li>
+            <li><a href="<?= BASE_URL ?>/gestione_cucina/elimina_piatto.php">Elimina un singolo piatto dal menu</a></li>
+            <li><a href="<?= BASE_URL ?>/gestione_cucina/modifica_piatto_00.php">Modifica un singolo piatto del menu</a></li>
+          </ul>
+        </div>
+
+        <div class="bg-bianco m-3 p-3 col-md-4 col-lg-3 col-11">
+          <h2 class="text-center">Gestione Eventi</h2>
+          <ul class="list-unstyled">
+            <li><a href="<?= BASE_URL ?>/gestione_eventi/crea_evento.php">Crea un nuovo evento</a></li>
+            <li><a href="<?= BASE_URL ?>/gestione_eventi/elimina_evento.php">Elimina un evento</a></li>
+            <li><a href="<?= BASE_URL ?>/gestione_eventi/modifica_evento_00.php">Modifica un evento</a></li>
+          </ul>
+        </div>
+
+        <div class="bg-bianco m-3 p-3 col-md-4 col-lg-3 col-11">
+          <h2 class="text-center">Gestione Utenti</h2>
+          <ul class="list-unstyled">
+            <li><a href="<?= BASE_URL ?>/gestione_utenti/crea_utente.php">Crea un nuovo utente con privilegi da Admin</a></li>
+            <li><a href="<?= BASE_URL ?>/gestione_utenti/elimina_utente.php">Elimina un utente</a></li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  
+<?php
+      }
+    }
+?>
+
+</main>
+
+<br>
 
 <!-- Funzione per creare dinamicamente il footer -->
 <?php HTMLfooter($nomePagina) ?>
